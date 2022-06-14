e04aa19ab410002c97863ec9ea010d06444913c3
Camila Senna Leite <89709135+CamilaSennaLeite@users.noreply.github.com>
2022-06-14 07:16:49 -0300

    Comentários sobre o código do desafio

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+//Essa função vai somar 1 ao meu número atual na contagem
+//++ é o mesmo que +1
 function increment() {
 	currentNumber = ++currentNumber;
 	currentNumberWrapper.innerHTML = currentNumber;
@@ -8,6 +10,8 @@ function increment() {
 	stopCount();
 }
 
+//Essa função vai diminuir 1 do meu número atual na contagem
+//-- é o mesmo que -1
 function decrement() {
 	currentNumber = --currentNumber;
 	currentNumberWrapper.innerHTML = currentNumber;
@@ -15,6 +19,7 @@ function decrement() {
 	stopCount();
 }
 
+//Essa função vai alterar as cores quando a contagem chegar na condição. 
 function refreshColor() {
 	if (currentNumber < 0) {
 		currentNumberWrapper.style.color = 'red';
@@ -23,9 +28,13 @@ function refreshColor() {
 	}  
   }
 
+//Essa função vai parar a contagem quanto o número for maior que 9+1 e quando for menor que 9-1
   function stopCount() {
 	if (currentNumber < -9) {
 		currentNumber = 0};
 	if (currentNumber > 9) {
 		currentNumber = 0};
   }
+
+/*Para que as funções de alterar cor (refreshColor) e parar contagem (stopColor) aconteçam é necessário chamá-las,
+dentro das funções de incremento e decremento.*/ 
